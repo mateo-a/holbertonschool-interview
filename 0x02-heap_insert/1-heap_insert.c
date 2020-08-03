852898f3872942e130b8abc766470c8d52c767b5
mateo-a <alexander.urrego@gmail.com>
2020-08-02 23:08:03 -0500

    [Fix] Task 1

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,134 +1,64 @@
 #include "binary_trees.h"
 /**
- * _height - Measures the height of a binary tree
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
+ * checkFree - Check the first free position in the tree
+ * @root: Pointer to the root node of the Heap
+ * Return: Pointer to parent with free position
  */
-int _height(const binary_tree_t *tree)
+heap_t *checkFree(heap_t *root)
 {
-	size_t height_l;
-	size_t height_r;
+	heap_t *arr[1000];
+	heap_t *strider = root;
+	int inPos = 0, outPos = 0;
 
-	if (!tree)
-		return (-1);
-
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
-}
-
-/**
- * heapify - Check if child is greather than parent and order the heap
- * @firstParent: Node parent of node to insert
- * @nodeChild: Node that will be inserted and need to be checked with
- *             the parent
- */
-void heapify(heap_t **firstParent, heap_t **nodeChild)
-{
-	heap_t *parentCheck, *childCheck, *nChild, *nodeLeft, *nodeRight, *parent;
-	int swp;
-
-	parentCheck = *firstParent, childCheck = *nodeChild;
-	if (childCheck->n > parentCheck->n)
+	while (strider && strider->left && strider->right)
 	{
-		if (childCheck->left)
-			childCheck->left->parent = parentCheck;
-		if (childCheck->right)
-			childCheck->right->parent = parentCheck;
-		if (parentCheck->left == childCheck)
-			nChild = parentCheck->right, swp = 0;
-		else
-			nChild = parentCheck->left, swp = 1;
-		nodeLeft = childCheck->left, nodeRight = childCheck->right;
-		if (swp == 0)
-		{
-			childCheck->right = nChild;
-			if (nChild)
-				nChild->parent = childCheck;
-			childCheck->left = parentCheck;
-		}
-		else
-		{
-			childCheck->left = nChild;
-			if (nChild)
-				nChild->parent = childCheck;
-			childCheck->right = parentCheck;
-		}
-		if (parentCheck->parent)
-		{
-			if (parentCheck->parent->left == parentCheck)
-				parentCheck->parent->left = childCheck;
-			else
-				parentCheck->parent->right = childCheck;
-		}
-		parent = parentCheck->parent, childCheck->parent = parent;
-		parentCheck->parent = childCheck, parentCheck->left = nodeLeft;
-		parentCheck->right = nodeRight, *firstParent = childCheck;
+		arr[inPos] = strider->left;
+		inPos += 1;
+		if (inPos >= 999)
+			inPos = 0;
+		arr[inPos] = strider->right;
+		inPos += 1;
+		if (inPos >= 999)
+			inPos = 0;
+		strider = arr[outPos];
+		outPos += 1;
+		if (outPos >= 999)
+			outPos = 0;
 	}
-}
-/**
- * treeFull - Check the height for the branches and if are full
- * @root: Pointer to the root
- * Return: 1 if tree is full otherwise 0
- */
-int treeFull(const binary_tree_t *root)
-{
-	if (root && _height(root->left) == _height(root->right))
-	{
-		if (_height(root->left) == -1)
-			return (1);
-		if ((root->left && !((root->left)->left) && !((root->left)->right))
-		&& (root->right && !((root->right)->left)
-		&& !((root->right)->right)))
-			return (1);
-		if (root && root->left && root->right)
-			return (treeFull(root->left) && treeFull(root->right));
-	}
-	return (0);
+	return (strider);
 }
 
 /**
- * heap_insert - function that inserts a value in Max Binary Heap
- * @root: double pointer to the root node of the Heap
+ * heap_insert - function that inserts a value into a Max Binary Heap
+ * @root: is a double pointer to the root node of the Heap
  * @value: is the value store in the node to be inserted
- * Return: A pointer to the inserted node, or NULL on failure
+ * Return: a pointer to the inserted node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new;
+	heap_t *posInsertion = NULL;
+	heap_t *new = NULL;
+	int temp;
 
+	if (root == NULL)
+		return (NULL);
 	if (*root == NULL)
 	{
 		*root = binary_tree_node(NULL, value);
 		return (*root);
 	}
-	if (treeFull(*root) || !treeFull((*root)->left))
-	{
-		if ((*root)->left)
-		{
-			new = heap_insert(&((*root)->left), value);
-			heapify(root, &((*root)->left));
-			return (new);
-		}
-		else
-		{
-			new = (*root)->left = binary_tree_node(*root, value);
-			heapify(root, &((*root)->left));
-			return (new);
-		}
-	}
-	if ((*root)->right)
-	{
-		new = heap_insert(&((*root)->right), value);
-		heapify(root, (&(*root)->right));
-		return (new);
-	}
+	posInsertion = checkFree(*root);
+	new = binary_tree_node(posInsertion, value);
+	if (posInsertion->left == NULL)
+		posInsertion->left = new;
 	else
+		posInsertion->right = new;
+	while (new->parent && new->n > (new->parent)->n)
 	{
-		new = (*root)->right = binary_tree_node(*root, value);
-		heapify(root, &((*root)->right));
-		return (new);
+		temp = (new->parent)->n;
+		(new->parent)->n = new->n;
+		new->n = temp;
+		new = new->parent;
 	}
-	return (NULL);
+	return (new);
 }
