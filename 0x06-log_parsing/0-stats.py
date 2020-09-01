64718f88a6ca66e6d543e3e60c6425bdb904892f
mateo-a <alexander.urrego@gmail.com>
2020-08-31 23:09:30 -0500

    [Fix] Task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python3
-"""
 
-Function that inserts a line of text to a file, after each line containing
-a specific string
+""" Script that reads stdin line by line and computes metrics """
 
-"""
+import sys
 
 
-def append_after(filename="", search_string="", new_string=""):
-    """
-    Inserts a line of text to a file, after each line containing a
-    specific string
-    Arguments:
-       filename (file): file
-       search_string (str): string
-       new_string (str): string
-    """
-    new = ""
-    with open(filename, mode="r+", encoding="utf-8") as f:
-        for line in f:
-            new += line
-            if search_string in line:
-                new += new_string
-        f.seek(0)
-        f.write(new)
+def printStatistics(statusCode, fileSize):
+    """ Function to print statistics """
+    print("File size: {:d}".format(fileSize))
+    for i in sorted(statusCode.keys()):
+        if statusCode[i] != 0:
+            print("{}: {:d}".format(i, statusCode[i]))
+
+
+statusCodes = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+statusCounter = 0
+fileSize = 0
+
+try:
+    for line in sys.stdin:
+        if statusCounter != 0 and statusCounter % 10 == 0:
+            printStatistics(statusCodes, fileSize)
+        statusList = line.split()
+        statusCounter += 1
+        try:
+            fileSize += int(statusList[-1])
+        except:
+            pass
+        try:
+            if statusList[-2] in statusCodes:
+                statusCodes[statusList[-2]] += 1
+        except:
+            pass
+    printStatistics(statusCodes, fileSize)
+
+except KeyboardInterrupt:
+    printStatistics(statusCodes, fileSize)
+    raise
