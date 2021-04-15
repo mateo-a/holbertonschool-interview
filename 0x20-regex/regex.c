10cc3c529322144422c73506af34865c4599c45f
mateo-a <alexander.urrego@gmail.com>
2021-04-15 10:24:07 -0500

    [Fix] Fix task

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,49 +1,6 @@
 #include "regex.h"
 
 /**
- * _com - compares string
- * @s1: string 1
- * @s2: string 2
- * @wild: wildcard flag
- *
- * Return: 1 if true otherwise 0
- */
-
-int _com(char const *s1, char const *s2, int wild)
-{
-	if (wild == 1)
-	{
-		if (s2[0] == '*')
-			return (_com(s1, s2 + 1, 1));
-		else if (s1[0] == s2[0])
-			return (_com(s1, s2, 0));
-		else if (s1[0] != '\0')
-			return (_com(s1 + 1, s2, 1));
-		else
-			return (0);
-	}
-	else
-	{
-		if (s1[0] == '\0' && s2[0] == '\0')
-			return (1);
-		else if (s1[0] == s2[0])
-		{
-			if (_com(s1 + 1, s2 + 1, 0))
-			{
-				return (_com(s1 + 1, s2 + 1, 0));
-			}
-		}
-		else if (s2[0] == '.' && s1[0] != '\0')
-			return (_com(s1 + 1, s2 + 1, 0));
-		else if (s2[0] == '*')
-			return (_com(s1, s2, 1));
-		else
-			return (0);
-	}
-	return (0);
-}
-
-/**
  * regex_match - checks whether a given pattern matches a given string.
  * @str: string
  * @pattern: pattern
@@ -53,5 +10,17 @@ int _com(char const *s1, char const *s2, int wild)
 
 int regex_match(char const *str, char const *pattern)
 {
-	return (_com(str, pattern, 0));
+	int tmp = 0;
+
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+	if ((*str == *pattern || *pattern == '.') && *(pattern + 1) != '*')
+		return (regex_match(str + 1, pattern + 1));
+	if (*(pattern + 1) == '*')
+	{
+		if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+			tmp = regex_match(str + 1, pattern);
+		return (regex_match(str, pattern + 2) || tmp);
+	}
+	return (0);
 }
